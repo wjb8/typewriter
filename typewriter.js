cfe64e880d4b6972cb0f1206b143118c1d1b860b
James Brown <brownj95@gmail.com>
2021-04-06 18:59:11 +0000

    Animated Sentence

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+  delay += 100;
+}
+setTimeout(() => {
+  process.stdout.write('\n');
+}, delay + 100);
